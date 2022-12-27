39f92c282247f33156713f38d1ee75047a5f188a
Abdul Khaleel <khaleelmca102@gmail.com>
2022-12-27 17:50:58 +0530

    added frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './css/style-min.css';
+import './css/custom.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Container from './components/Container';
+
+function App() {
+  return (
+    <div>
+        <Header />
+        <Container />
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
